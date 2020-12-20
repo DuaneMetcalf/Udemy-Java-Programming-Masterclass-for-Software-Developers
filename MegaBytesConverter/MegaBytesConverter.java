1461a693b9b5151d8a2d9accf6c1d63f0dcf8407
DuaneMetcalf <15147695+DuaneMetcalf@users.noreply.github.com>
2020-12-20 13:43:53 +0000

    MegaBytes Converter
    Added description and code for the MegaBytes converter exercise

---
+++ MegaBytesConverter/MegaBytesConverter.java
@@ -0,0 +1,13 @@
+public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int totalMegaBytes = kiloBytes / 1024;
+            int remainingKiloBytes = totalMegaBytes % kiloBytes;
+            System.out.println(kiloBytes + " KB = " + totalMegaBytes + " MB and " + remainingKiloBytes + " KB");
+        }
+    }
+}
